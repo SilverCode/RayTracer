ff84a16757a7c209a50b70fe36c7997985545314
Ross Simpson <ross@onedigitalmedia.com>
2016-04-08 17:36:31 +0200

    Add camera class

--- vec3.h
+++ vec3.h
@@ -36,6 +36,11 @@ class vec3 {
 
 };
 
+inline vec3& operator+=(const vec3 &v1, const vec3 &v2) const {
+	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+
 inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
 	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]); 
 }
